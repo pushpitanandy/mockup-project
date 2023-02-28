4044680635f81000befe389436cc13f6d7612752
pushpitanandy <nandypushpita@gmail.com>
2023-02-28 19:28:28 -0400

    photos and details sections created

---
+++ src/components/PhotoGallery/PhotoGallery.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './PhotoGallery.css';
+
+const PhotoGallery = () => {
+    return (
+        <div className='photo-gallery'>
+            <div className='photos-section'>
+                <h1>Photos</h1>
+            </div>
+            <div className='photo-details'>
+                <h3>Detail section</h3>
+            </div>
+        </div>
+    );
+};
+
+export default PhotoGallery;
