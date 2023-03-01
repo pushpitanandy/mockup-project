a2b1c7ec5b1966b69b96b2481d287f7d1d9a7b8c
pushpitanandy <nandypushpita@gmail.com>
2023-02-28 22:35:49 -0400

    navbar created

--- src/components/PhotoGallery/PhotoGallery.js
+++ src/components/PhotoGallery/PhotoGallery.js
@@ -4,9 +4,16 @@ import './PhotoGallery.css';
 const PhotoGallery = () => {
     return (
         <div className='photo-gallery'>
+            {/* photos section */}
             <div className='photos-section'>
                 <h1>Photos</h1>
+                <nav className='navbar'>
+                    <a href="/recent">Recently Added</a>
+                    <a href="/favorited">Favorited</a>
+                </nav>
             </div>
+           
+            {/* photo detail section */}
             <div className='photo-details'>
                 <h3>Detail section</h3>
             </div>
